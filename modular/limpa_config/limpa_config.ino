f0faf0937b2defa4b012a9b40fa1a067c9892aae
rede.analista <66534023+rede-analista@users.noreply.github.com>
2024-01-02 15:15:10 -0300

    Limpa configurações (Não executa format)
    Limpa configurações específicas (Não executa format). Limpa Pinos, Ações e Intermódulos

---
+++ modular/limpa_config/limpa_config.ino
@@ -0,0 +1,44 @@
+#include <Preferences.h>
+#include <nvs_flash.h>
+
+/*---- Inicia objetos ----*/
+Preferences CONFIG_FLASH;
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  Serial.println("");
+
+  Serial.print("As preferencias estao sendo limpas, todas as configuracoes serão perdidas... ");
+  CONFIG_FLASH.begin("confiGeral", false);
+      CONFIG_FLASH.remove("paS_InterMod");
+
+      CONFIG_FLASH.remove("paU8_Pinos");
+      CONFIG_FLASH.remove("paS8_Pinos");
+
+      CONFIG_FLASH.remove("paU16_Acao1");
+      CONFIG_FLASH.remove("paU16_AcaoRede1");
+      CONFIG_FLASH.remove("paS8_Acao1");
+      CONFIG_FLASH.remove("paU16_Acao2");
+      CONFIG_FLASH.remove("paU16_AcaoRede2");
+      CONFIG_FLASH.remove("paS8_Acao2");
+      CONFIG_FLASH.remove("paU16_Acao3");
+      CONFIG_FLASH.remove("paU16_AcaoRede3");
+      CONFIG_FLASH.remove("paS8_Acao3");
+  CONFIG_FLASH.end();
+  delay(100);
+  Serial.println(" OK");
+
+
+  Serial.println("OK");
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  delay(1000);
+}
